1a1e2f685c132184c6c7612b81bffeb224bfca52
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-04-23 16:56:28 -0300

    refactor: appRoutes to use effector-react for state management

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useUnit } from "effector-react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { HomePage } from "../pages/home";
 import { SignInPage } from "../pages/signIn";
+import { LoginStore } from "../stores/signIn/store";
 
 export const AppRoutes = () => {
+  const { userEmail } = useUnit(LoginStore);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<SignInPage />} />
+        <Route
+          path="/"
+          element={userEmail ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!userEmail ? <SignInPage /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
